103351f6a741121691f6ef0f528156c6efef9b9c
Sarthak Burmi <87921529+sarthak-burmi@users.noreply.github.com>
2024-02-12 00:44:56 +0530

    final_changes

--- lib/services/functions.dart
+++ lib/services/functions.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/services.dart';
-import 'package:voting_app/utils/constants.dart';
 import 'package:web3dart/web3dart.dart';
+
+import 'package:voting_app/utils/constants.dart';
 
 Future<DeployedContract> loadContract() async {
   String abi = await rootBundle.loadString('assets/abi.json');
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 String infura_url =
-    "https://sepolia.infura.io/v3/6038dbe997414e598adf54e8648d730c";
+    "https://sepolia.infura.io/v3/0c60ad9dc3b44163955b658bb95787db";
 String owner_private_key =
     "ba7edb0a6219988c412879d2486902e7b4c2ebb5056e0a1d2aad398d6505c7c9";
 String voter_private_key =
     "a37a659b3c1bb9d1b6bf77492da42518381eb1ae5dd799258341b5b748370ba0";
-String contractAddress1 = "0x35f2D2bC20786Cc2C013536E7980705549abfB0D";
+String contractAddress1 = "0x8AdC75ccE8b4Be77cE68eeB3AAf16d19e35f54F8";
